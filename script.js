917cd58fba0966819c82a0394bf7599abd78d044
Evan Owenby <89050989+evanonb@users.noreply.github.com>
2021-12-09 03:38:37 +0000

    fixed console log errors

--- script.js
+++ script.js
@@ -3,16 +3,33 @@ var usrInput = document.getElementById("totalprice");
 var slider = document.getElementById("sliderrange");
 var percentOutput = document.getElementById("displayslidervalue");
 var totalOutput = document.getElementById("tipTotal");
-percentOutput.innerHTML = slider.value+"%"; // Display the default slider value
 
-slider.oninput = function() {
+ // Display the default slider value
+//function onload() {
+//  percentOutput.innerHTML = this.value+"%";
+//}
+
+/*slider.onload = function1() {
   percentOutput.innerHTML = this.value+"%";
-}
+}*/
+
+
+
+/*percentOutput = function() {
+  percentOutput.innerHTML = "22"//slider.value+"%";
+}*/
+
+// currency formatter
+var formatter = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD',
+});
 
 // calc buttons
 function tipCalc() {
   var total = usrInput.value * slider.value/100;
-  document.getElementById("tipTotal").innerHTML = total;
+  var roundedTotal = 
+  document.getElementById("tipTotal").innerHTML = formatter.format(total);
 }
 
 function clearAll() {
